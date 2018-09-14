2101616ffe0aebcf76ca902c7bd8035e0c7d4efb
Abdul baki <abdul.baki@ft.com>
2018-09-14 12:00:18 +0100

    pycodestyle compliance changes

--- tests/python_version_verifier_test.py
+++ tests/python_version_verifier_test.py
@@ -10,31 +10,31 @@ import python_version_verifier
 from python_version_verifier import require_python_3_6
 
 
-@pytest.mark.parametrize("py_version", [ (3, 6), (3, 7) ])
+@pytest.mark.parametrize("py_version", [(3, 6), (3, 7)])
 def test_requirement_satisfied_with_python_version_3_6_or_higher(py_version):
     require_python_3_6(py_version)
 
 
-@pytest.mark.parametrize("py_version", [ (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5) ])
+@pytest.mark.parametrize("py_version", [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5)])
 def test_requirement_is_not_satisfied_with_python_versions_prior_to_3_6(py_version):
     with pytest.raises(Exception):
         require_python_3_6(py_version)
 
 
-@pytest.mark.parametrize("py_version", [ (2, 6), (2, 7) ])
+@pytest.mark.parametrize("py_version", [(2, 6), (2, 7)])
 def test_requirement_is_not_satisfied_with_major_version_2(py_version):
     with pytest.raises(Exception):
         require_python_3_6(py_version)
 
 
-@pytest.mark.parametrize("py_version", [ (4, 0) ])
+@pytest.mark.parametrize("py_version", [(4, 0)])
 def test_requirement_satisfied_with_python_version_3_6_or_higher(py_version):
     with pytest.raises(Exception):
         require_python_3_6(py_version)
 
 
-@pytest.mark.parametrize("required_major,required_minor", [ (3, 6) ])
-@pytest.mark.parametrize("py_version", [ (4, 0), (2, 6), (3, 5) ])
+@pytest.mark.parametrize("required_major,required_minor", [(3, 6)])
+@pytest.mark.parametrize("py_version", [(4, 0), (2, 6), (3, 5)])
 def test_error_message_returned_on_exception(py_version, required_major, required_minor):
     with pytest.raises(Exception) as exception_resp:
         require_python_3_6(py_version)
@@ -46,8 +46,8 @@ def test_error_message_returned_on_exception(py_version, required_major, require
     assert str(exception_resp.value) == expected_error_msg
 
 
-@pytest.mark.parametrize("required_major,required_minor", [ (3, 6) ])
-@pytest.mark.parametrize("py_version", [ (3, 5) ])
+@pytest.mark.parametrize("required_major,required_minor", [(3, 6)])
+@pytest.mark.parametrize("py_version", [(3, 5)])
 def test_required_parameters_returned_on_exception(py_version, required_major, required_minor):
     with pytest.raises(Exception) as exception_resp:
         require_python_3_6(py_version)
@@ -59,7 +59,7 @@ def test_required_parameters_returned_on_exception(py_version, required_major, r
     assert str(exception_resp.value) == expected_error_msg
 
 
-@pytest.mark.parametrize("required_major,required_minor", [ (3, 6), (3, 5) ])
-@pytest.mark.parametrize("py_version", [ (3, 6), (3, 7) ])
+@pytest.mark.parametrize("required_major,required_minor", [(3, 6), (3, 5)])
+@pytest.mark.parametrize("py_version", [(3, 6), (3, 7)])
 def test_required_parameters_return_successful(py_version, required_major, required_minor):
     require_python_3_6(py_version, required_major, required_minor)
