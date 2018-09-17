f05998c81d3f121adae16b472ba5cd4640bb19db
Abdul baki <abdul.baki@ft.com>
2018-09-17 14:08:55 +0100

    moved try catch

--- python_version_verifier/python_version_verifier.py
+++ python_version_verifier/python_version_verifier.py
@@ -3,32 +3,30 @@ from functools import wraps
 
 
 def require_python_3_6(python_version, required_major=3, required_minor=6):
+    try:
+        major_version = python_version[0]
+        minor_version = python_version[1]
 
-    major_version = python_version[0]
-    minor_version = python_version[1]
+        error_msg = 'Detected Python {0}.{1}. '\
+            'Python {2}.{3} or above is required, '\
+            'please update python to v{2}.{3} or higher'\
+            .format(major_version, minor_version, required_major, required_minor)
 
-    error_msg = 'Detected Python {0}.{1}. '\
-        'Python {2}.{3} or above is required, '\
-        'please update python to v{2}.{3} or higher'\
-        .format(major_version, minor_version, required_major, required_minor)
+        if (major_version != required_major):
+            raise Exception(error_msg)
 
-    if (major_version != required_major):
-        raise Exception(error_msg)
+        if (minor_version < required_minor):
+            raise Exception(error_msg)
 
-    if (minor_version < required_minor):
-        raise Exception(error_msg)
-
-    print(python_version)
-
+        print(python_version)
+    except Exception as err:
+        print("{}".format(err))
+        raise err
 
 def python_3_6_handler(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
-        try:
-            py_version = sys.version_info
-            require_python_3_6(python_version=py_version)
-            function(*args, **kwargs)
-        except Exception as err:
-            print("{}".format(err))
-            raise err
+        py_version = sys.version_info
+        require_python_3_6(python_version=py_version)
+        function(*args, **kwargs)
     return wrapper
