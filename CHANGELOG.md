9c0781e16aad732e3ba79f8d313498323617671c
Andrew Grant <andrew.grant@ft.com>
2018-10-17 16:21:42 +0200

    Version 0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+[0.0.2]
+Andrew Grant, Mon, 8 Oct 2018 13:57:20 +0200
+    rework routine
+
+Andrew Grant, Mon, 1 Oct 2018 11:35:17 +0200
+    update ref details
+
+abdulbaki-ft, Thu, 27 Sep 2018 11:29:17 +0100
+    fixed wrapper not returning function (#12)
+
+[0.0.2]
+Andrew Grant, Mon, 8 Oct 2018 13:57:20 +0200
+    rework routine
+
+Andrew Grant, Mon, 1 Oct 2018 11:35:17 +0200
+    update ref details
+
+abdulbaki-ft, Thu, 27 Sep 2018 11:29:17 +0100
+    fixed wrapper not returning function (#12)
+
--- python_version_verifier/__init__.py
+++ python_version_verifier/__init__.py
@@ -1,1 +1,19 @@
+"""Init Package."""
 from .python_version_verifier import *
+
+__all__ = (
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+)
+
+__title__ = "python_version_verifier"
+__summary__ = "A python 3 verions checking library for python functions"
+__uri__ = "https://github.com/Financial-Times/python-version-verifier"
+
+__version__ = "0.0.2"
+
+__author__ = "Financial Times - Cloud Enablement Team"
+__email__ = "cloudenablement@ft.com"
+
+__license__ = "MIT License"
+__copyright__ = "Copyright 2018"
