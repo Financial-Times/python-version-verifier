61ee8e9253f0ce0ff4dedfa88fd87c6bcf44edc0
Andrew Grant <andrew.grant@ft.com>
2018-10-23 10:27:34 +0200

    add classifier

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     url=python_version_verifier.__uri__,
     packages=setuptools.find_packages(),
     classifiers=(
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
